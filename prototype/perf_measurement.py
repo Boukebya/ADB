828c580fa7857d7be5869e7504e8923a2f7f000a
Yanks <62990521+Boukebya@users.noreply.github.com>
2023-11-09 14:54:37 +0100

    Minor fixes of performance measurement

--- prototype/perf_measurement.py
+++ prototype/perf_measurement.py
@@ -5,16 +5,35 @@ def word_method(list1, list2):
     """
 
     counter = 0
+    # order list
+    list1.sort()
+    list2.sort()
+
+    #print(list1)
+    #print(list2)
+
+    #print("List1 :",len(list1), "List2 :", len(list2))
+
+    no_match = []
     # take each word in google_ocr and find it in tess_ocr
     for word in list1:
         # if the word is the same, add 1 to the counter
         if word in list2:
             counter += 1
-            # delete the word from the list to avoid duplicates
+            # remove the word from the list
             list2.remove(word)
+
+        else:
+            no_match.append(word)
             list1.remove(word)
 
-    return counter / len(list1)
+    #print("Words that are still in lists (no matching) :",len(no_match))
+    #print(no_match)
+    #print("lists:")
+    #print(list1)
+    #print(list2)
+
+    return counter / (len(list1))
 
 
 def split_text(text):
@@ -36,6 +55,7 @@ def split_text(text):
             # If words anything else than characters, numbers or punctuation, skip it
             if word.isalnum() and word.isascii():
                 words_list.append(word)
+
     return words_list
 
 
@@ -48,11 +68,12 @@ def compare_ocr(google_ocr, tess_ocr):
 
     google_words = split_text(google_ocr)
     tess_words = split_text(tess_ocr)
-    print(google_words)
-    print(tess_words)
 
     score = word_method(google_words, tess_words)
-    print(score)
+    print(score, ", This score represents how much of the words in list 1 are present in list 2."
+                 "\nNote that the score is measured by finding the same words in the two lists, not the same order"
+                 "\nbased on the first list. If the first list is longer than the second, the score will be lower"
+                 "\nbecause the first list is the reference.")
     return score
 
 
