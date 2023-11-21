74e8aa5f6c1b9fc14b578503854fc20410f3dbf9
Yanks <62990521+Boukebya@users.noreply.github.com>
2023-11-21 17:04:13 +0100

    Performance for opencv and gpt3

--- src/Performance_and_evaluation/Vertex_gpt3.py
+++ src/Performance_and_evaluation/Vertex_gpt3.py
@@ -1,6 +1,7 @@
 import time
 
 from src.Extraction.gpt3_extraction import gpt3_extraction
+from src.OCR.opencv_ocr import opencv_ocr
 from src.OCR.vertex_ocr import vertex_ocr
 from src.Performance_and_evaluation.perf_measurement import compare_ocr
 
@@ -19,6 +20,18 @@ def test_gptvertex(file_path,file_verif,file_extract):
     return score, score_extract
 
 
+def test_gptopencv(file_path,file_verif,file_extract):
+    # use vertex ocr
+    start_time = time.time()
+    opencv_ocr(file_path)
+    score = compare_ocr("ocr.txt",file_verif)
+    gpt3_extraction("ocr.txt")
+    score_extract = compare_ocr(file_extract,"ocr.txt")
+    print("%s seconds to achieve both things" % (time.time() - start_time))
+    print("Score vertex :", score)
+    print("Score opencv :", score_extract)
 
+    return score, score_extract
 
-data1 = test_gptvertex("../../images/1.jpg","../Furniture_list/data_1.txt","../Furniture_list/data_extraction1.txt")
+data1 = test_gptopencv("../../images/1.jpg","../Furniture_list/data_1.txt","../Furniture_list/data_extraction1.txt")
+#data1 = test_gptvertex("../../images/1.jpg","../Furniture_list/data_1.txt","../Furniture_list/data_extraction1.txt")
